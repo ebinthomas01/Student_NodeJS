bdbd37c09afd4789bb9d5370d7ea68dd3eb0a9b0
ebinthomas01 <ebintkm@gmail.com>
2024-06-07 21:57:50 +0530

    Day 3 work finished

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,105 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const ViewAll = () => {
+    const [data,changeData]=useState(
+        [
+        {"name":"aju", "admNo":1014, "rollNo":1},
+		    {"name":"anu", "admNo":1025, "rollNo":2},
+		    {"name":"manu", "admNo":1403, "rollNo":3},
+            {"name":"ebin", "admNo":1035, "rollNo":4},
+            {"name":"kiya", "admNo":1995, "rollNo":5},
+            {"name":"mohan", "admNo":1225, "rollNo":6}
+    
+        ]
+    )
   return (
+
     <div>
         
         <NavBar/>
         <div className="container">
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row">
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <img src="https://images.filmibeat.com/ml/img/2011/10/29-antony-perumbavoor.jpg" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Antony</p>
-                                <p class="card-text">Roll No: 24</p>
+                    <div className="row g-3">
+                        {
+                            data.map(
+                                (value, index)=>{return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                    <br></br>
+                                    <div class="card">
+                                        <img src="https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/passport/1-change1.jpg" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                        <p class="card-text">Admission No :{value.admNo}</p>
+                                        <p class="card-text">Name :{value.name}</p>
+                                        <p class="card-text">Roll No :{value.rollNo}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://img.nowrunning.com/content/Artist/RoshanBasheer/banner.jpg" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Varun Prabakaran</p>
-                                <p class="card-text">Roll No: 15</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSK--GoPBwriT3tDbIl4V6uPXRm3AxSf7XqpNTnKVpvfbNSm7J5" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Anju Georgekutty</p>
-                                <p class="card-text">Roll No: 54</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ7cTCc4T2CruzG6VlNJTcCNUFw7xSeso59O9xhhYKoDHo2Tjrh" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Anu Georgekutty</p>
-                                <p class="card-text">Roll No: 47</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://media.tenor.com/Y3YxhA0SPWQAAAAe/vasu-vasu-annan.png" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Vasu</p>
-                                <p class="card-text">Roll No: 14</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://static.langimg.com/thumb/81224000/samayam-malayalam-81224000.jpg?imgsize=84473&width=540&resizemode=3" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Saritha</p>
-                                <p class="card-text">Roll No: 34</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://i.pinimg.com/736x/6e/c4/1f/6ec41f6e41c369e77705ba976dbe5bd8.jpg" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">GeorgeKutty</p>
-                                <p class="card-text">Roll No: 84</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://www.indiancinemagallery.net/wp-content/photos/celebs/asha-sarath/Asha-Sarath-at-AMMA-general-body-meeting-2018-5.jpg" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Geetha Prabakaran</p>
-                                <p class="card-text">Roll No:52</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://in.bmscdn.com/iedb/artist/images/website/poster/large/murali-gopy-31057-24-03-2017-12-55-12.jpg" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Thomas</p>
-                                <p class="card-text">Roll No: 33</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQIb_kAT28I-z9_eJs5J3-518YdwJfraL3GA&s" height="400px" class="card-img-top" alt="..."></img>
-                                <div class="card-body">
-                                <p class="card-text">Prabakar</p>
-                                <p class="card-text">Roll No: 37</p>
-                                </div>
-                            </div>
-                        </div>
+                                }
+                            )
+                        }
                     </div>
                 </div>
             </div>
